4c54b2fa56613e1af400b9ae6a44ac23e301dddb
John <angelo.casiano@bluehive.com.ph>
2023-12-12 14:57:33 +0800

    dec-12-2023

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,7 +114,7 @@ exports.restrictTo = (...roles) => {
     // roles ['admin', 'lead-guide'], role='user'
     if (!roles.includes(req.user.role)) {
       return next(
-        new AppError('You do not have permission to perform this action', 403),
+        new AppError('You do not have permission to perform this action', 404),
       );
     }
     next();
